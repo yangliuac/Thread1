5e6b885c7ad2ce2322c15ac1b7468e58dc38b1e7
yangliuac <liuy210202@163.com>
2017-09-28 22:03:38 +0800

    hello gitHub Server

--- src/test06/Run.java
+++ src/test06/Run.java
@@ -8,6 +8,6 @@ public class Run {
 		myThread.start();
 //		Thread.sleep(1000);
 		System.out.println("end =="+myThread.isAlive());
-		System.out.println();
+		System.out.println("hello gitHub Server");
 	}
 }
